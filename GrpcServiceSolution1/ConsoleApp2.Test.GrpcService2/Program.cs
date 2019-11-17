5e99fbd11c8823460e4ea039c250c85fbd2f25b2
Nikita Temnikov <nistoc@gmail.com>
2019-11-17 20:51:16 +0300

    Initial commit containing a very first simple test of the technology

---
+++ GrpcServiceSolution1/ConsoleApp2.Test.GrpcService2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Reflection.Metadata.Ecma335;
+using System.Threading.Tasks;
+using Grpc.Net.Client;
+using GrpcService2;
+
+namespace ConsoleApp2.Test.GrpcService2
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            WriteLine("Hello World!");
+
+            Write("Tell me your name:");
+            var name = ReadLine();
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new Greeter.GreeterClient(channel);
+
+            var response = await client.SayHelloAsync(new HelloRequest{Name = name});
+            WriteLine(response.Message);
+            ReadLine();
+        }
+
+        private static string ReadLine() => Console.ReadLine();
+        private static void Write(string text) => Console.Write(text);
+        private static void WriteLine(string text) => Console.WriteLine(text);
+    }
+}
